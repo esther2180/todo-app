1b37a531dd813e3c93a251d3364638c4bc3abd99
Esther <esther2180@gmail.com>
2018-09-21 13:07:24 -0700

    Update API Server

--- src/components/app.js
+++ src/components/app.js
@@ -1,55 +1,115 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React, { Component } from 'react';
+import axios from 'axios';
 import AddItem from './add_item';
 import List from './list';
 import dummyListData from '../dummy_data/list_data';
 
 //This is the owner of the data controls the data!
 
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=c718todolistes';
+
 class App extends Component {
     state = {
-        list: []  //data is an array so set to empty array
+        list: [],  //data is an array so set to empty array
+        error: ''   //to display errors to users
     }
     
     componentDidMount() {
         this.getListData();
     }
     
-    getListData() {
+    async getListData() {
         // Call server to get data
-    
-        this.setState({
-            list: dummyListData
-        });
+        // http://api.reactprototypes.com/todos?key=somekey;
+
+        try {
+            const resp = await axios.get(`${BASE_URL}/todos${API_KEY}`);
+
+            if(!resp.data.success){
+                throw new Error('Something went wrong!');
+            }
+
+            this.setState({
+                list: resp.data.todos
+            });
+
+        } catch(err) {
+            console.log('Get Data Error:', err.message);
+
+            this.setState({
+                error: 'Error retrieving list data'
+            });
+        }
+
+        // const resp = axios.get(`${BASE_URL}/todos${API_KEY}`).then((resp) => {
+        //     // console.log('Server Resp:', resp);
+        //     this.setState({
+        //         list: resp.data.todos
+        //     });
+        // }).catch((err) => {
+        //     console.log('Get List Eata Error:', err.message);
+
+        //     this.setState({
+        //         error: 'Error retrieving list data'
+        //     });
+        // });
+        // // console.log('Axios Return Value:', resp);
     }
     
-    addItem = (item) => {
-        item._id = new Date().getTime();
+    //NEWER WAY - using async
+    addItem = async (item) => {  
+        await axios.post(`${BASE_URL}/todos${API_KEY}`, item);  
 
-        this.setState({
-            list: [item, ...this.state.list]
-        });
+        this.getListData();
+
+        //First
+        // item._id = new Date().getTime();
+
+        // this.setState({
+        //     list: [item, ...this.state.list]
+        // });
+
+        //OLDER WAY
+        // axios.post(`${BASE_URL}/todos${API_KEY}`, item).then((resp) => {  //bind with fat arrow function
+        //     console.log('Add Item Resp: ', resp);
+
+        //     this.getListData();  //recall to update from server and show on dom
+        // }).catch((err) => {
+        //     console.log('Add Item Error:', err);
+        // });
+
     }
 
-    deleteItem = index => {
-        const {list} = this.state;
+    deleteItem = async id => {
+        console.log('Delete Item ID:', id);
 
-        const listCopy = list.slice();  //makes a copy of an array
+        await axios.delete(`${BASE_URL}/todos/${id + API_KEY}`);
 
-        listCopy.splice(index, 1);  //remove from that copy array
+        this.getListData();
 
-        this.setState({
-            list: listCopy  //reset the array list
-        });
+        //OLD WAY
+        // const {list} = this.state;
+
+        // const listCopy = list.slice();  //makes a copy of an array
+
+        // listCopy.splice(index, 1);  //remove from that copy array
+
+        // this.setState({
+        //     list: listCopy  //reset the array list
+        // });
     }
 
     render() {
-        const { list } = this.state;
+        const { list, error } = this.state;
 
+        console.log('List:', list);
         return (
             <div className="container">
                 <h1 className="center">To Do App</h1>
                 <AddItem add={this.addItem} />
+                <p className="red-text">{error}</p>
                 <List data={list} delete={this.deleteItem} />
             </div>
         );
--- src/components/list.js
+++ src/components/list.js
@@ -4,7 +4,7 @@ import Item from './item';
 
 const List = (props) => {
     const listElements = props.data.map((item, index) => {
-        return <Item key={item._id} item={item} delete={() => props.delete(index)} />  // do not use index use unique id to set the value to its key
+        return <Item key={item._id} item={item} delete={() => props.delete(item._id)} />  // do not use index use unique id to set the value to its key
     });
 
     return (  //no need to put it in a div everything, just return ul or just return form or button etc.
